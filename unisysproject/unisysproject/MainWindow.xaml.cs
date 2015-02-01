ca90982b114e75d87e5a5571085e95686cc7ec03
ashwin <ashwin.naresh94@gmail.com>
2015-02-01 20:07:00 +0530

    client for pre-finals
    has little backend for most of arjun's pages

---
+++ unisysproject/unisysproject/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace unisysproject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Window1.xaml", UriKind.Relative)); //for navigation to work, it should be a page implementation, not window
+        }
+    }
+}
